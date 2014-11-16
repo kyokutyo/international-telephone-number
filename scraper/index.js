b5f31ce633cf82f39e2053449accf16e946a0cdc
kyokutyo <haruta@kyokutyo.com>
2014-11-16 20:03:25 +0900

    名前を変える

--- scraper/index.js
+++ scraper/index.js
@@ -2,23 +2,24 @@
     'use strict';
 
     var client = require('cheerio-httpcli');
-    var url = 'http://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E9%9B%BB%E8%A9%B1%E7%95%AA%E5%8F%B7%E3%81%AE%E4%B8%80%E8%A6%A7';
-    var str = [];
+    var url    = 'http://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E9%9B%BB%E8%A9%B1%E7%95%AA%E5%8F%B7%E3%81%AE%E4%B8%80%E8%A6%A7';
+    var str    = [];
 
     client.fetch(url, function (err, $, res) {
         $('table th code').each(function () {
-            var $code = $(this);
+            var $code   = $(this);
             var $parent = $code.parent();
             var tmp_str = '';
 
             tmp_str += '{"code":"' + $code.text() + '",';
             $parent.siblings('td').each(function(index) {
-                var $td = $(this);
+                var $td  = $(this);
+                var text = $td.text().replace(/\s+\[\d+\]/g, '');
 
                 if(index === 0) {
-                    tmp_str += '"country":"' + $td.text().replace(/\s+\[\d+\]/g, '');
-                } else if($td.text() !== '') {
-                    tmp_str += '(' + $td.text() + ')';
+                    tmp_str += '"name":"' + text;
+                } else if(text !== '') {
+                    tmp_str += '(' + text + ')';
                 }
             });
             tmp_str += '"}';
