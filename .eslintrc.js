3c917431f397c2495d841b57d966b37768316cfb
kyokutyo <kyokutyo@gmail.com>
2020-02-10 22:50:23 +0900

    Convert to function components

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,9 @@ module.exports = {
     "plugins": [
         "react"
     ],
-    "rules": {
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
     }
 };
---
+++ public/static/js/components/Country.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+
+export const Country = props => (
+  <div className="country">
+    <span
+      className="code"
+      dangerouslySetInnerHTML={{ __html: props.code }}
+    />
+    <span className="name">{props.name}</span>
+  </div>
+);
+
+Country.propTypes = {
+  code: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+}
